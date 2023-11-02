44cccf15f693f975e7ed45cde39f0d5c1297f5bc
Norayr Chilingarian <norayr@arnet.am>
2023-11-02 13:21:58 +0400

    pushing

---
+++ notes/compilation_stages/modules_and_makefile/make/c/test.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "kyoto.h"
+//int add (int a, int b);
+
+int main()
+{
+
+  int i, j, k;
+  i = 40; j = 2;
+  k = add (i, j);
+  printf ("res = %d\n", k);
+  return 0;
+
+
+}
---
+++ notes/rpn/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   a = 5;
+   if (a = 7) {
+
+      printf ("true!\n");
+
+   }
+
+    return 0;
+
+}
